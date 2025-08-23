b76b4f7291c1762c980ce1b0d540a6e3225d3c72
LeePapa <61931198+LeePapa@users.noreply.github.com>
2025-08-23 16:09:21 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -130,9 +130,9 @@ UPDATE_VERSION "tailscale"
 #else
 #  echo "未找到 luci-app-passwall2/Makefile，请确认当前目录正确。"
 #fi
-sed -i 's@include ../../lang/rust/rust-package.mk@include ../lang/rust/rust-package.mk@g' openwrt-smartdns/Makefile
+sed -i 's@include ../../lang/rust/rust-package.mk@include ../feeds/packages/lang/rust/rust-package.mk@g' openwrt-smartdns/Makefile
 echo "==== openwrt-smartdns/Makefile 内容如下 ===="
 cat openwrt-smartdns/Makefile
 echo "==== Makefile 内容结束 ===="
-ls ../
+ls ../feeds/
 echo "==== ls 内容结束 ===="
