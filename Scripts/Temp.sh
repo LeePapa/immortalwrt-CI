be839c7af5f456437880d12f4deeec48397eb08b
LeePapa <61931198+LeePapa@users.noreply.github.com>
2025-08-26 01:40:18 +0800

    Update Temp.sh

--- Scripts/Temp.sh
+++ Scripts/Temp.sh
@@ -7,7 +7,7 @@ echo "==== Cat Makefile ===="
 cat $WORKINGDIR/Makefile
 echo "==== Cat Makefile End===="
 echo "==== Tree ===="
-tree ../
+ls ../
 echo "==== Tree End===="
 mkdir -p $WORKINGDIR
 rm -fr $WORKINGDIR/*
